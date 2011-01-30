041d82f6921f373b580598308ce1dba847a18ad2
Michael Portuesi <mportuesisf@gmail.com>
2011-01-30 13:59:01 -0800

    New icon; display natural screen orientation; add version number to about box; now installs on SD card.

--- src/com/jotabout/screeninfo/ScreenInfo.java
+++ src/com/jotabout/screeninfo/ScreenInfo.java
@@ -3,6 +3,10 @@ package com.jotabout.screeninfo;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -27,15 +31,22 @@ public class ScreenInfo extends Activity {
         setContentView(R.layout.main);
         
         showDeviceInfo();
-        showScreenInfo();
+        showScreenMetrics();
+        showDefaultOrientation();
     }
     
+    /**
+     * Show basic information about the device.
+     */
     public void showDeviceInfo() {
         ((TextView) findViewById(R.id.device_name)).setText( Build.MODEL );
         ((TextView) findViewById(R.id.os_version)).setText( Build.VERSION.RELEASE );
     }
     
-    public void showScreenInfo() {
+    /**
+     * Show the screen metrics (pixel dimensions, density, dpi, etc) for the device.
+     */
+    public void showScreenMetrics() {
 		WindowManager wm = ((WindowManager) getSystemService(Context.WINDOW_SERVICE));
 		Display display = wm.getDefaultDisplay();
     	
@@ -51,7 +62,17 @@ public class ScreenInfo extends Activity {
         ((TextView) findViewById(R.id.logical_density)).setText( Double.toString(metrics.density) );
         ((TextView) findViewById(R.id.font_scale_density)).setText( Float.toString(metrics.scaledDensity) );
         
-    	// Screen physical diagonal size in inches, rounded to one place after decimal (e.g. '3.7', '10.1')
+    	showScreenDiagonalSize(metrics);
+    }
+
+    /**
+     * Calculate and display the physical diagonal size of the screen.
+     * The size is calculated in inches, rounded to one place after decimal (e.g. '3.7', '10.1')
+     * The size is also calculated in millimeters
+     * 
+     * @param metrics
+     */
+	private void showScreenDiagonalSize(DisplayMetrics metrics) {
 		double xdpi = metrics.xdpi;
 		if ( xdpi < 1.0 ) {
 			// Guard against divide-by-zero, possible with lazy device manufacturers who set these fields incorrectly
@@ -63,15 +84,54 @@ public class ScreenInfo extends Activity {
 			ydpi =  metrics.densityDpi;
 		}
 		
+		// Calculate physical screen width/height
 		double physicalWidth = ((double) metrics.widthPixels) / xdpi;
 		double physicalHeight = ((double) metrics.heightPixels) / ydpi;
 		
+		// Calculate diagonal screen size, in both U.S. and Metric units
 		double rawDiagonalSizeInches = Math.sqrt(Math.pow(physicalWidth, 2) + Math.pow(physicalHeight, 2));
 		double diagonalSizeInches = Math.floor( rawDiagonalSizeInches * 10.0 + 0.5 ) / 10.0;
 		double diagonalSizeMillimeters = Math.floor( rawDiagonalSizeInches * 25.4 + 0.5 );
         ((TextView) findViewById(R.id.computed_diagonal_size_inches)).setText( Double.toString(diagonalSizeInches) );
         ((TextView) findViewById(R.id.computed_diagonal_size_mm)).setText( Double.toString(diagonalSizeMillimeters) );
-    }
+	}
+
+	/**
+	 * Display the "natural" screen orientation of the device.
+	 */
+	private void showDefaultOrientation() {
+		// Screen default orientation
+        TextView orientationText = ((TextView) findViewById(R.id.orientation));
+        Configuration config = getResources().getConfiguration();
+        switch ( config.orientation ) {
+        case Configuration.ORIENTATION_LANDSCAPE:
+        	orientationText.setText(R.string.orientation_landscape);
+        	break;
+        case Configuration.ORIENTATION_PORTRAIT:
+        	orientationText.setText(R.string.orientation_portrait);
+        	break;
+        case Configuration.ORIENTATION_SQUARE:
+        	orientationText.setText(R.string.orientation_square);
+        	break;
+        case Configuration.ORIENTATION_UNDEFINED:
+        	orientationText.setText(R.string.orientation_undefined);
+        	break;
+        }
+	}
+	
+	/**
+	 * Return a string containing version number from the package manifest.
+	 */
+	private String appVersion() {
+		String version = "";
+		PackageInfo info;
+		try {
+			info = getPackageManager().getPackageInfo(getPackageName(), 0);
+			version = this.getString(R.string.version) + " " + info.versionName;
+		} catch (NameNotFoundException ignore) {;}
+
+		return version;
+	}
 
 	@Override
 	protected Dialog onCreateDialog(int id) {
@@ -82,6 +142,7 @@ public class ScreenInfo extends Activity {
 	        mAbout = new Dialog(this);
 	        mAbout.setContentView(R.layout.about_dialog);
 	        mAbout.setTitle(R.string.about_title);
+	        ((TextView) mAbout.findViewById(R.id.about_version)).setText(appVersion());
 	        ((Button) mAbout.findViewById(R.id.about_dismiss)).setOnClickListener(new View.OnClickListener() {
 				@Override
 				public void onClick(View v) {
